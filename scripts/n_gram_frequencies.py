0b25aa84a9a9e8e7c9d9569127cf4cffda2656c6
Lea <leakoedel19@gmail.com>
2025-05-30 17:02:04 +0200

    Approach on handling frequencies

--- scripts/n_gram_frequencies.py
+++ scripts/n_gram_frequencies.py
@@ -1,7 +1,7 @@
-from nltk.corpus import PlaintextCorpusReader
-from nltk.probability import FreqDist
 import tokenization
 import n_gram
+from nltk.corpus import PlaintextCorpusReader
+from nltk.probability import FreqDist
 
 corpus_root = '/home/lea_k/language_detection_project/Language-Detection-Application/data/training/german'
 corpus = PlaintextCorpusReader(corpus_root, '.*\.txt')
@@ -16,4 +16,10 @@ tokens = tokenization.tokenizeWithPadding(corpus.raw('1.txt'))
 three_grams = n_gram.compute_n_gram(tokens, 3)
 # Compute frequency distribution
 freq_dist = FreqDist(three_grams)
-print(f"Frequenzverteilung für {'1.txt'}:\n{freq_dist.most_common(10)}\n")
+print(f"Frequency distribution for {'1.txt'}:\n{freq_dist.most_common(20)}\n")
+
+tokens = tokenization.tokenizeWithPadding(corpus.raw('1.txt'))
+three_grams = n_gram.compute_n_gram(tokens, 4)
+# Compute frequency distribution
+freq_dist = FreqDist(three_grams)
+print(f"Frequency distribution for {'1.txt'}:\n{freq_dist.most_common(20)}\n")
