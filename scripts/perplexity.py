3e0eb9848dfb3ecb4647f41a4094c11a4efa7b42
Lea <leakoedel19@gmail.com>
2025-06-04 14:32:51 +0200

    update

--- scripts/perplexity.py
+++ scripts/perplexity.py
@@ -3,6 +3,7 @@ import json
 from n_gram import compute_n_gram
 import tokenization
 import n_gram_frequencies
+from nltk.corpus import PlaintextCorpusReader
 
 def calculate_perplexity(test_tokens, model_probs, n, smoothing=1e-4):
     n_grams = compute_n_gram(test_tokens, n)
@@ -15,18 +16,23 @@ def calculate_perplexity(test_tokens, model_probs, n, smoothing=1e-4):
 
     return 2 ** (-log_prob_sum / N)
 
-with open("Language-Detection-Application/json_data/german/3grams.json") as f:
+
+"""with open("Language-Detection-Application/json_data/german/3grams.json") as f:
     model_3gram = json.load(f)
 
 with open("Language-Detection-Application/json_data/german/4grams.json") as f:
     model_4gram = json.load(f)
 
+corpus_root = 'Language-Detection-Application/data/development/german'
 
+corpus = PlaintextCorpusReader(corpus_root, '.*\.txt')
 
-tokenized_test = tokenization.tokenizeWithPadding('Language-Detection-Application/data/development/german/dev1.txt')
+text = corpus.raw('dev1.txt')
+tokenized_test = tokenization.tokenizeWithPadding(text)
 
 perplexity_3 = calculate_perplexity(tokenized_test, model_3gram, n=3)
 perplexity_4 = calculate_perplexity(tokenized_test, model_4gram, n=4)
 
 print("3-Gram Perplexity:", perplexity_3)
-print("4-Gram Perplexity:", perplexity_4)
+print("4-Gram Perplexity:", perplexity_4)"""
+
