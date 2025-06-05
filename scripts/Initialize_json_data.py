7de37ede71b156bcc878c15c1ab1d16636620656
Lea <leakoedel19@gmail.com>
2025-06-05 17:13:27 +0200

    initialize json data of english and italian

--- scripts/Initialize_json_data.py
+++ scripts/Initialize_json_data.py
@@ -1,9 +1,10 @@
 import n_gram_frequencies
 import tokenization
 
-corpus_root = '/home/lea_k/language_detection_project/Language-Detection-Application/data/training/german'
+corpus_root = '/home/lea_k/language_detection_project/Language-Detection-Application/data/training/english'
 
 tokenized_text = tokenization.tokenizeWithPadding(n_gram_frequencies.GetWholeText(corpus_root))
 
 # Training data is already injected into the file
-#n_gram_frequencies.SaveAsJSON(n_gram_frequencies.ComputeRelativeFrequencies(tokenized_text, 3), 'Language-Detection-Application/json_data/german/3grams.json')
+#n_gram_frequencies.SaveAsJSON(n_gram_frequencies.ComputeRelativeFrequencies(tokenized_text, 3), 'Language-Detection-Application/json_data/english/3grams.json')
+#n_gram_frequencies.SaveAsJSON(n_gram_frequencies.ComputeRelativeFrequencies(tokenized_text, 4), 'Language-Detection-Application/json_data/english/4grams.json')
