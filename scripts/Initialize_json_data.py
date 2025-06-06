eb764aa1e022d7ddb014f2dcc7a0cccb13d101b2
Lea <leakoedel19@gmail.com>
2025-06-06 15:31:58 +0200

    updated data

--- scripts/Initialize_json_data.py
+++ scripts/Initialize_json_data.py
@@ -7,4 +7,6 @@ tokenized_text = tokenization.tokenizeWithPadding(n_gram_frequencies.GetWholeTex
 
 # Training data is already injected into the file
 n_gram_frequencies.SaveAsJSON(n_gram_frequencies.ComputeRelativeFrequencies(tokenized_text, 2), 'Language-Detection-Application/json_data/italian/2grams.json')
+n_gram_frequencies.SaveAsJSON(n_gram_frequencies.ComputeRelativeFrequencies(tokenized_text, 3), 'Language-Detection-Application/json_data/italian/3grams.json')
+n_gram_frequencies.SaveAsJSON(n_gram_frequencies.ComputeRelativeFrequencies(tokenized_text, 4), 'Language-Detection-Application/json_data/italian/4grams.json')
 n_gram_frequencies.SaveAsJSON(n_gram_frequencies.ComputeRelativeFrequencies(tokenized_text, 5), 'Language-Detection-Application/json_data/italian/5grams.json')
